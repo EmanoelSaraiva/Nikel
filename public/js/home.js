0bf14a7a2490cb25416a98d6a30ac6baa96f7905
EmanoelSaraiva <emanoelsaraivas@gmail.com>
2022-12-30 09:34:17 -0300

    Iniciando o Git

---
+++ public/js/home.js
@@ -0,0 +1,165 @@
+const myModal = new bootstrap.Modal("#transactionModal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+let data = {
+    transactions: []
+};
+
+document.getElementById("transactions-button").addEventListener("click", function(){
+    window.location.href = "transaction.html";
+});
+document.getElementById("button-logout").addEventListener("click", logout);
+
+//ADICIONAR LANÇAMENTO
+document.getElementById("transaction-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("input-description").value;
+    const date = document.getElementById("input-date").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        value: value, type: type, descriptrion: description, date: date
+    });
+
+    saveData(data);
+
+    e.target.reset();
+    myModal.hide();
+
+    getCashIn();
+    getCashOut();
+    getTotal();
+    alert("Lançamento adicionado com sucesso.");
+
+});
+checkLogged();  
+
+function checkLogged(){
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(!logged){
+        window.location.href = "index.html"
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser){
+        data = JSON.parse(dataUser);
+    }
+
+    getCashIn();
+    getCashOut();
+    getTotal();
+}
+
+
+function logout(){
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+
+    
+}
+
+function getCashIn(){
+    const transaction = data.transactions;
+
+    const cashIn = transaction.filter((item) => item.type === "1");
+
+    if(cashIn.length){
+        let cashInHtml = ``;
+        let limit = 0;
+
+        if(cashIn.length > 5) {
+            limit = 5;
+        }else{
+            limit = cashIn.length;
+        }
+        for (let index = 0; index < limit; index++) {
+            cashInHtml += `
+               <div class="row mb-4">
+                   <div class="col-12">
+                       <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
+                       <div class="container p-0">
+                           <div class="row">
+                               <div class="col-12 col-md-8">
+                                   <p>${cashIn[index].descriptrion}</p>
+                               </div>
+                               <div class="col-12 col-md-3 justify-content-end">
+                                   ${cashIn[index].date}
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           `
+
+           document.getElementById("cash-in-list").innerHTML = cashInHtml;
+       }
+    }
+        
+}
+
+function getCashOut(){
+    const transaction = data.transactions;
+
+    const cashIn = transaction.filter((item) => item.type === "2");
+
+    if(cashIn.length){
+        let cashInHtml = ``;
+        let limit = 0;
+
+        if(cashIn.length > 5) {
+            limit = 5;
+        }else{
+            limit = cashIn.length;
+        }
+        for (let index = 0; index < limit; index++) {
+            cashInHtml += `
+               <div class="row mb-4">
+                   <div class="col-12">
+                       <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
+                       <div class="container p-0">
+                           <div class="row">
+                               <div class="col-12 col-md-8">
+                                   <p>${cashIn[index].descriptrion}</p>
+                               </div>
+                               <div class="col-12 col-md-3 justify-content-end">
+                                   ${cashIn[index].date}
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           `
+
+           document.getElementById("cash-out-list").innerHTML = cashInHtml;
+       }
+    }
+        
+}
+
+function getTotal(){
+    const transactions = data.transactions;
+    let total = 0;
+
+    transactions.forEach((item) => {
+        if(item.type === "1"){
+            total += item.value;
+        } else {
+            total -= item.value;
+        }
+    });
+
+    document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`
+}
+
+function saveData(data){
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
---
+++ public/js/index.js
@@ -0,0 +1,101 @@
+const myModal = new bootstrap.Modal("#registerModal")
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+
+    checkLogged();
+
+//LOGAR NO SISTEM
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const senha = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account){
+        alert("Opps! Verifique o usuário ou a senha.");
+        return;
+    }
+
+    if(account){
+        if(account.password !== senha){
+            alert("Opps! Verifique o usuário ou a senha.")
+            return;
+        }
+
+
+        saveSession(email, checkSession);
+
+        window.location.href = "home.html";
+
+    }    
+
+})
+
+//CRIA CONTA
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    const email = document.getElementById("creator-input-email").value;
+    const senha = document.getElementById("creator-input-password").value;
+
+    if(email.length < 5){
+        alert("Prencha o campo com um e-mail valido");
+        return;
+    }
+
+    if(senha.length < 4){
+        alert("Preencha a senha com no mínimo 4 digitos!");
+        return;
+    }   
+
+    saveAccount({
+        login: email,
+        password: senha,
+        transactions: []
+    })
+
+
+    myModal.hide();    
+
+    alert("Conta criada com sucesso!");
+});
+
+function checkLogged(){
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(logged){
+        saveSession(logged, session);
+
+        window.location.href = "home.html"
+    }
+}
+
+function saveAccount(data){
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key)
+
+    if(account){
+        return JSON.parse(account);
+    }
+
+    return "";
+}
+
